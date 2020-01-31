08348e294a1f786b59079275cfa08b9fff1d7b11
malika65 <malika.satimbaeva@iaau.edu.kg>
2020-01-31 10:01:54 +0600

    first commit

---
+++ src/main/java/HelloWorld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "HEllo , from me";
+    }
+}
